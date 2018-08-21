758d37ce8ca53f97ab9c4c2e814de8a4ae5c56c8
A.Rupesh <a.rupesh66@gmail.com>
2018-08-21 23:47:13 +0530

    Create Apicall.js

---
+++ src/Apicall.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Apicall extends Component {
+    render(){
+        return(
+
+            <div>
+            I am from Api call
+
+            </div>
+
+
+        );
+    }
+}
+
+
+export default Apicall;
